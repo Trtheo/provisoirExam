79fe57a6ee7c105149d9ef9716a3c23b30c6a74b
Mr Theophile NIYIGABA <117538022+Trtheo@users.noreply.github.com>
2023-09-30 09:17:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,320 @@
+const quizData = [
+  {
+    question: 'Ikinyabiziga cyose cyangwa ibinyabiziga bigenda bigomba kugira:',
+    options: [
+      'Umuyobozi',
+    'Umuherekeza',
+    'A na B ni ibisubizo byukuri',
+    'Nta gisubizo cyukuri kirimo'],
+    answer: 'Umuyobozi'
+  },
+  {
+    question: 'Ijambo "akayira" bivuga inzira nyabagendwa ifunganye yagenewe gusa:',
+    options: ['Abanyamaguru', 
+    'Ibinyabiziga bigendera ku biziga bibiri ',
+     'A na B ni ibisubizo byukuri',
+      'Nta gisubizo cyukuri kirimo'],
+    answer: 'A na B ni ibisubizo byukuri',
+  },
+  {
+    question: 'Umurongo uciyemo uduce umenyesha ahegereye umurongo ushobora kuzuzwa nuturanga gukata twibara ryera utwo turanga cyerekezo tumenyesha :',
+    options: [
+      'Igisate cyumuhanda abayobozi bagomba gukurikira', 
+    'Ahegereye umurongo ukomeje',
+     'Igabanurwa ryumubare wibisate byumuhanda mu cyerekezo bajyamo',
+      'A na C nibyo'],
+    answer: 'Igabanurwa ryumubare wibisate byumuhanda mu cyerekezo bajyamo ',
+  },
+  {
+    question: 'Ahantu ho kugendera mu muhanda herekanwa nibimenyetso bimurika ibinyabiziga ntibishobora kuhagenda :',
+    options: [' Biteganye', 
+    'Ku murongo umwe', 
+    'A na B nibyo', 
+    'Nta gisubizo cyukuri kirimo'],
+    answer: ' Nta gisubizo cyukuri kirimo',
+  },
+  {
+    question: 'Ibinyabiziga bikurikira bigomba gukorerwa isuzumwa buri mwaka:',
+    options: [
+      'Ibinyabiziga bigenewe gutwara abagenzi muri rusange',
+      'Nta gisubizo cyukuri kirimo',
+      'Ibinyabiziga bigenewe gutwara ibintu birengeje toni 3.5',
+      'Ibinyabiziga bigenewe kwigisha gutwara',
+    ],
+    answer: 'Nta gisubizo cyukuri kirimo',
+  },
+  {
+    question: 'Icyapa cyerekana umuvuduko ntarengwa ikinyabiziga kitagomba kurenza gishyirwa gusa ku binyabiziga bifite uburemere ntarengwa bukurikira:',
+    options: ['Burenga toni 1', 
+    'Burenga toni 2',
+     'Burenga toni 24',
+      'Nta gisubizo cyukuri kirimo'],
+    answer: 'Nta gisubizo cyukuri kirimo',
+  },
+  {
+    question: 'Ubugari bwa romoruki ikuruwe nikinyamitende itatu ntibugomba kurenza ibipimo bikurikira:',
+    options: [
+      'cm 75 ',
+      'cm 125 ',
+      'cm 265',
+      'Nta gisubizo cyukuri',
+    ],
+    answer: 'Nta gisubizo cyukuri',
+  },
+  {
+    question: 'Kunyuranaho bikorerwa:',
+    options: [' Mu ruhande rwiburyo gusa',
+     'Igihe cyose ni ibumoso', 
+     'Iburyo iyo unyura ku nyamaswa',
+      'Nta gisubizo cyukuri kirimo'],
+    answer: 'Nta gisubizo cyukuri kirimo',
+  },
+  {
+    question: 'Uburebure bwibinyabiziga bikurikira ntibugomba kurenga metero 11 :',
+    options: [
+      'Ibifite umutambiko umwe uhuza imipira',
+      'Ibifite imitambiko ibiri ikurikiranye mu bugari bwayo',
+      'Nta gisubizo cyukuri',
+      'Makuzungu',
+    ],
+    answer: 'Nta gisubizo cyukuri',
+  },
+  {
+    question: 'Ikinyabiziga kibujijwe guhagarara akanya kanini aha hakurikira :',
+    options: ['Ahatarengeje metero 1 imbere cyangwa inyuma yikinyabiziga gihagaze akanya gato cyangwa kanini', 
+    'Ahantu hari ibimenyetso bibuza byabugenewe',
+     'Aho abanyamaguru banyura mu muhanda ngo bakikire inkomyi',
+      'Ibisubizo byose nibyo'],
+    answer: 'Ibisubizo byose nibyo',
+  },
+ {
+  question: 'hatari mu nsisiro umuvuduko ntarengwa mu isaha wa velomoteri ni: ',
+    options: [
+      'Km 50',
+      'Km 40',
+      ' Km 30',
+      'Nta gisubizo cyukuri',
+    ],
+    answer: 'Km 50',
+  },
+
+  {
+    question: 'Ku byerekeye kwerekana ibinyabiziga nukumurika kwabyo ndetse no kwerekana ihindura ryibyerekezo byabyo. Birabujijwe gukora andi matara cyangwa utugarurarumuri uretse ibitegetswe ariko ntibireba amatara akurikira:',
+      options: [
+        'Amatara ndanga',
+        'Amatara ari imbere mu modoka  ',
+        'Amatara ndangaburambarare',
+        'Ibisubizo byose nibyo',
+      ],
+      answer: 'Amatara ari imbere mu modoka ',
+    },
+    {
+      question: 'Iyo  nta mategeko awugabanya byumwihariko umuvuduko ntarengwa wamapikipiki mu isaha ni:',
+        options: [
+          'Km 25',
+          'Km 70  ',
+          'Km 40',
+          'Nta gisubizo cyukuri kirimo',
+        ],
+        answer: 'Nta gisubizo cyukuri kirimo',
+      },
+      {
+        question: 'Uburyo bukoreshwa kugirango ikinyabiziga kigende gahoro igihe feri idakora neza babwita:     ',
+          options: [
+            'Feri yurugendo',
+            'Feri yo guhagarara umwanya munini  ',
+            'Feri yo gutabara',
+            'Nta gisubizo cyukuri kirimo',
+          ],
+          answer: 'Feri yo gutabara',
+        },
+        {
+          question: ' Nibura ikinyabiziga gitegetswe kugira uduhanagurakirahure tungahe:',
+            options: [
+              '2 ',
+              '3',
+              '1',
+              'Nta gisubizo cyukuri kirimo',
+            ],
+            answer: '1',
+          },
+          {
+            question: 'Amatara maremare yikinyabiziga agomba kuzimwa mu bihe bikurikira:            ',
+              options: [
+                'Iyo umuhanda umurikiye umuyobozi abasha kureba muri metero 20',
+                ' Iyo ikinyabiziga kigiye kubisikana nibindi',
+                'Iyo ari mu nsisiro',
+                'Ibisubizo byose ni ukuri',
+              ],
+              answer: ' Iyo ikinyabiziga kigiye kubisikana nibindi',
+            },
+
+            {
+              question: 'Ikinyabiziga ntigishobora kugira amatara arenga abiri yubwoko bumwe keretse kubyerekeye amatara akurikira: ',
+                options: [
+                  'Itara ndangamubyimba',
+                  ' Itara ryerekana icyerekezo',
+                  'Itara ndangaburumbarare',
+                  'Ibisubizo byose ni ukuri',
+                ],
+                answer: 'Ibisubizo byose ni ukuri',
+              },
+              {
+       question: 'Ibinyabiziga bikoreshwa nka tagisi, bitegerereza abantu mu nzira nyabagendwa, bishobora gushyirwaho itara ryerekana ko ikinyabiziga kitakodeshejwe. Iryo tara rishyirwaho ku buryo bukurikira:',
+                  options: [
+                    'Ni itara ryicyatsi rishyirwa imbere ku kinyabiziga',
+                    'Ni itara ryicyatsi rishyirwa ibumoso',
+                    'Ni itara ryumuhondo rishyirwa inyuma',
+                    'A na C ni ibisubizo byukuri',
+                  ],
+                  answer: 'Ni itara ryicyatsi rishyirwa imbere ku kinyabiziga',
+                },
+                {
+                  question: 'Za otobisi zagenewe gutwara abanyeshuri zishobora gushyirwaho amatara abiri asa nicunga rihishije amyasa kugirango yerekane ko zihagaze no kwerekana ko bagomba kwitonda, ayo matara ashyirwaho ku buryo bukurikira :',
+                    options: [
+                      'Amatara abiri ashyirwa inyuma',
+                      'Amatara abiri ashyirwa imbere',
+                      'Rimwe rishyirwa imbere irindi inyuma',
+                      'b na c ni ibisubizo byukuri',
+                    ],
+                    answer: 'Rimwe rishyirwa imbere irindi inyuma',
+                  },
+                  {
+                    question: 'Iyo umuvuduko wibinyabiziga bidapakiye ushobora kurenga km50 mu isaha ahategamye, bigomba kuba bifite ibikoresho byihoni byumvikanira mu ntera:',
+                      options: [
+                        'Metero 100',
+                        'Metero 200',
+                        'Metero 50',
+                        ' Metero 150 ',
+                      ],
+                      answer: 'Metero 50',
+                    }
+];
+
+const quizContainer = document.getElementById('quiz');
+const resultContainer = document.getElementById('result');
+const submitButton = document.getElementById('submit');
+const retryButton = document.getElementById('retry');
+const showAnswerButton = document.getElementById('showAnswer');
+
+let currentQuestion = 0;
+let score = 0;
+let incorrectAnswers = [];
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function displayQuestion() {
+  const questionData = quizData[currentQuestion];
+
+  const questionElement = document.createElement('div');
+  questionElement.className = 'question';
+  questionElement.innerHTML = questionData.question;
+
+  const optionsElement = document.createElement('div');
+  optionsElement.className = 'options';
+
+  const shuffledOptions = [...questionData.options];
+  shuffleArray(shuffledOptions);
+
+  for (let i = 0; i < shuffledOptions.length; i++) {
+    const option = document.createElement('label');
+    option.className = 'option';
+
+    const radio = document.createElement('input');
+    radio.type = 'radio';
+    radio.name = 'quiz';
+    radio.value = shuffledOptions[i];
+
+    const optionText = document.createTextNode(shuffledOptions[i]);
+
+    option.appendChild(radio);
+    option.appendChild(optionText);
+    optionsElement.appendChild(option);
+  }
+
+  quizContainer.innerHTML = '';
+  quizContainer.appendChild(questionElement);
+  quizContainer.appendChild(optionsElement);
+}
+
+function checkAnswer() {
+  const selectedOption = document.querySelector('input[name="quiz"]:checked');
+  if (selectedOption) {
+    const answer = selectedOption.value;
+    if (answer === quizData[currentQuestion].answer) {
+      score++;
+    } else {
+      incorrectAnswers.push({
+        question: quizData[currentQuestion].question,
+        incorrectAnswer: answer,
+        correctAnswer: quizData[currentQuestion].answer,
+      });
+    }
+    currentQuestion++;
+    selectedOption.checked = false;
+    if (currentQuestion < quizData.length) {
+      displayQuestion();
+    } else {
+      displayResult();
+    }
+  }
+}
+
+function displayResult() {
+  quizContainer.style.display = 'none';
+  submitButton.style.display = 'none';
+  retryButton.style.display = 'inline-block';
+  showAnswerButton.style.display = 'inline-block';
+  resultContainer.innerHTML = `Wagize amanota: ${score} / ${quizData.length}`;
+}
+
+function retryQuiz() {
+  currentQuestion = 0;
+  score = 0;
+  incorrectAnswers = [];
+  quizContainer.style.display = 'block';
+  submitButton.style.display = 'inline-block';
+  retryButton.style.display = 'none';
+  showAnswerButton.style.display = 'none';
+  resultContainer.innerHTML = '';
+  displayQuestion();
+}
+
+function showAnswer() {
+  quizContainer.style.display = 'none';
+  submitButton.style.display = 'none';
+  retryButton.style.display = 'inline-block';
+  showAnswerButton.style.display = 'none';
+
+  let incorrectAnswersHtml = '';
+  for (let i = 0; i < incorrectAnswers.length; i++) {
+    incorrectAnswersHtml += `
+      <p style="background:#80baed;">
+        <strong>IKIBAZO:</strong> ${incorrectAnswers[i].question}<br>
+        <strong style="color:green;">IGISUBIZO WATANZE:</strong> ${incorrectAnswers[i].incorrectAnswer}<br>
+        <strong style="color:white;">IGISUBIZO CY'UKURI:</strong> ${incorrectAnswers[i].correctAnswer}
+      </p>
+    `;
+  }
+
+  resultContainer.innerHTML = `
+    <p style="background:#80baed;color:white">WAGIZE AMANOTA:&nbsp;&nbsp;&nbsp${score} / ${quizData.length}</p>
+    <p style="color:red"><u>IBISUBIZO BITARI UKURI:</u></p>
+    ${incorrectAnswersHtml}
+  `;
+}
+
+submitButton.addEventListener('click', checkAnswer);
+retryButton.addEventListener('click', retryQuiz);
+showAnswerButton.addEventListener('click', showAnswer);
+
+displayQuestion();
+
+
+
+    
